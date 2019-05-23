f9c9fbc01dfc61839692e0d1a5a96b8b245f62fd
Christy Peters Ilobah <ilobahxty@gmail.com>
2019-05-23 20:40:58 +0100

    get and post http request

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { nextContext } from '@angular/core/src/render3';
 
 @Component({
   selector: 'app-profile',
@@ -8,16 +9,31 @@ import { HttpClient } from '@angular/common/http';
 })
 export class ProfileComponent implements OnInit {
   departments
+  chats
+  newMessage = {
+    avatar: "im4.jpg",
+    msg: "welcome to my chat group",
+    nickname: "baloxty",
+    sender_id: "59ad58b9a0f6cf030ddea4f2"
+  }
+  page = 0
+  limit = 3
 
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
     this.getDepartments()
+    this.getChats()
   }
 
   responseMethod = (data) => {
     this.departments = data.result
-    console.log(data)
+    // console.log(data)
+  }
+
+  chatMethod = (chat) => {
+  this.chats = chat
+  console.log(chat)
   }
 
   errorMethod = (error) => {
@@ -25,10 +41,33 @@ export class ProfileComponent implements OnInit {
   }
 
   getDepartments() {
-    this.http.get('https://smart-clinic.smartapps.com.ng/v1/department').subscribe(
+    this.http.get(`https://smart-clinic.smartapps.com.ng/v1/department?page=${this.page}&limit=${this.limit}`).subscribe(
       this.responseMethod,
       this.errorMethod
-      )
+    );
   }
 
+  nextPage(){
+    this.page = this.page + 1
+    this.getDepartments()
+  }
+
+
+  getChats() {
+    this.http.get('https://bashed.herokuapp.com/router/messages').subscribe(
+      this.chatMethod,
+      this.errorMethod
+    )
+  }
+
+  postNewMessage(){
+    this.http.post('https://bashed.herokuapp.com/router/postmsg', this.newMessage).subscribe(
+      (res) => {
+        console.log(res)
+      },
+      (err) => {
+        console.log(err)
+      }
+    )
+  }
 }
